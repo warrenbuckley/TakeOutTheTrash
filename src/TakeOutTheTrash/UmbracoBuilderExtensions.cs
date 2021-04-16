e87dd39e7b4b95d9d96bd22ec57f944047bfed14
Warren Buckley <warren@umbraco.com>
2021-04-16 13:13:57 +0100

    Add in UmbracoBuilder extension method to wire everything up for our package - that the composer is also calling

---
+++ src/TakeOutTheTrash/UmbracoBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace TakeOutTheTrash
+{
+    public static class UmbracoBuilderExtensions
+    {
+        public static IUmbracoBuilder AddTakeOutTheTrashPackage(this IUmbracoBuilder builder)
+        {
+            // Register any services or any notification handlers (events) here
+
+            // EXAMPLE - Add to Umbraco collection of things
+            // builder.Sections().Append<MyAwesomeSection>()
+            // builder.Dashboards().Add<MyFabDashboard>()
+
+            // EXAMPLE - Custom config 
+            // builder.AddConfiguration
+
+            // BackgroundTasks
+            // RecurringHostedServiceBase Background Task stuff is added as HostedService
+            builder.Services.AddHostedService<CleanRoom>();
+
+            return builder;
+        } 
+    }
+}
